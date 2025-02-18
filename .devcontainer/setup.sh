4210995c9ec5a17b59682ac32a43ec1b47115679
Riccardo Balbo <riccardo0blb@gmail.com>
2025-02-18 13:58:43 +0000

    mount docker socket for integration test

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,7 @@ CONTAINER_WORKSPACE_FOLDER=$1
 cd $CONTAINER_WORKSPACE_FOLDER
 sudo apt update -y
 sudo apt install -y curl
+sudo apt-get install -y docker.io
 
 curl -fsSL https://deb.nodesource.com/setup_20.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh

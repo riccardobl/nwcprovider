35a4d3ed12b7a64593437fd5e51b0bf332114546
Riccardo Balbo <riccardo0blb@gmail.com>
2025-02-19 18:55:17 +0000

    update integration tests

--- tests/integration/start.sh
+++ tests/integration/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 # cd in the script folder
 cd "$(dirname "$0")"
 
@@ -27,18 +27,25 @@ ghcr.io/hoytech/strfry:latest
 # Start lnbits with the nwcprovider extension
 rm -Rf lnbits_itest_data
 unzip data.zip
+
 docker run --name=lnbits_nwcprovider_ext_lnbits_test \
 -d \
 --rm \
+--user $(id -u):$(id -g) \
 -p 5002:5000 \
 -v ${PWD}/.env:/app/.env \
--v ${PWD}/lnbits_itest_data/:/app/data \
--v ${PWD}/../../:/app/lnbits/extensions/nwcprovider:ro \
-lnbits/lnbits
-
+-v ${PWD}/lnbits_itest_data/:/data \
+-v ${PWD}/../../:/nwcprovider \
+-v ${PWD}/../../.devcontainer/start.sh:/start-lnbits.sh:ro \
+-v ${PWD}/../../.devcontainer/setup.sh:/setup.sh:ro \
+mcr.microsoft.com/devcontainers/python:1-3.12 bash -c "while true; do sleep 1000; done"
 
 docker network create lnbits_nwcprovider_ext_test_network || true
 docker network connect lnbits_nwcprovider_ext_test_network lnbits_nwcprovider_ext_nostr_test --alias nostr|| true
 docker network connect lnbits_nwcprovider_ext_test_network lnbits_nwcprovider_ext_lnbits_test --alias lnbits|| true
 
-
+docker exec lnbits_nwcprovider_ext_lnbits_test  bash -c "curl -sSL https://install.python-poetry.org | python3 -"
+docker exec lnbits_nwcprovider_ext_lnbits_test bash -c "export PATH=\"/home/vscode/.local/bin:\$PATH\" && bash /setup.sh /nwcprovider"
+docker exec lnbits_nwcprovider_ext_lnbits_test bash -c "ln -s /app/.env \$HOME/lnbits/.env"
+docker exec -d lnbits_nwcprovider_ext_lnbits_test bash -c "export PATH=\"/home/vscode/.local/bin:\$PATH\" && cd \$HOME/lnbits && poetry run lnbits"
+ 

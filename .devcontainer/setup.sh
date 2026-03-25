cc821b9910bd18ec2059ff3a4857f81ca808dea0
Riccardo Balbo <os@rblb.it>
2026-03-25 16:56:41 +0100

    update dev environment and workaround issue on devcontainer image

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,11 @@ echo $PYTHONPATH
 CONTAINER_WORKSPACE_FOLDER=$1
 cd $CONTAINER_WORKSPACE_FOLDER
 
+# workaround for devimage
+sudo find /etc/apt/sources.list.d -maxdepth 1 -type f -exec \
+    sh -c 'grep -q "dl.yarnpkg.com/debian" "$1" && rm -f "$1" || true' _ {} \;
+sudo sed -i '/dl.yarnpkg.com\/debian/d' /etc/apt/sources.list || true
+
 cd $HOME
 echo $PWD
 if [ ! -d ./lnbits ] ; then
@@ -25,3 +30,4 @@ cd $CONTAINER_WORKSPACE_FOLDER
 poetry install --no-interaction
 npm i prettier
 npm i pyright
+pip install uv
